db9fdb4c08c7446f2502ba06200817543a42c140
root <root@ci-cd.us-central1-a.c.climalaboral.internal>
2021-01-02 06:34:30 +0000

    df

---
+++ nginx-static/static/precache-manifest.19bcc3ddc8136bcca2d86f046d604b72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b353b9428400d509b64333b51cb53e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66489330134bdb162e19",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "5493e9b0938854e8ca29",
+    "url": "/static/css/main.7a1af78e.chunk.css"
+  },
+  {
+    "revision": "66489330134bdb162e19",
+    "url": "/static/js/2.ddf3462d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ddf3462d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5493e9b0938854e8ca29",
+    "url": "/static/js/main.ce60fc2d.chunk.js"
+  },
+  {
+    "revision": "1ee3f02f01f46ffe1680",
+    "url": "/static/js/runtime-main.e21afe43.js"
+  },
+  {
+    "revision": "209c1afff101c7a731dee798a90b4f18",
+    "url": "/static/media/logoUsac.209c1aff.png"
+  }
+]);
